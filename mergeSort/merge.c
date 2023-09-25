84fbb7f66aa170e2b964c6a258ecf2e351942f8b
matheus <matheusmw21@gmail.com>
2023-09-25 19:10:16 -0300

    fix: merge_sorted function

--- mergeSort/merge.c
+++ mergeSort/merge.c
@@ -8,7 +8,7 @@ void merge_sort_recursion(int arr[], int l, int r);
 
 void merge_sorted(int arr[], int l, int m, int r);
 
-void merge_sort(int arr[], int length){
+void merge_sort(int arr[], int length) {
 
     merge_sort_recursion(arr, 0, length - 1);
 }
@@ -19,7 +19,7 @@ void merge_sort_recursion(int arr[], int l, int r) {
         int m = l + (r-1) / 2;
 
         merge_sort_recursion(arr, l, m); // porção esquerda do array
-        merge_sort_recursion(arr, m + 1, r); // porão direita do array
+        merge_sort_recursion(arr, m + 1, r); // porção direita do array
 
         merge_sorted(arr, l, m, r);
     }
@@ -30,33 +30,31 @@ void merge_sorted(int arr[], int l, int m, int r) {
     int left_length = m - l + 1;
     int right_length = r - m;
 
-    int *left = (int *)malloc(left_length * sizeof(int));
-    int *right = (int *)malloc(right_length * sizeof(int));
+    int temp_left[left_length];
+    int temp_right[right_length];
 
     int i, j, k;
 
     for(int i = 0; i < left_length; i++) {
-        left[i] = arr[l + 1];
+        temp_left[i] = arr[l + 1];
     }
 
     for (int i = 0; i < right_length; i++) {
-        right[i] = arr[m + 1 + i];
+        temp_right[i] = arr[m + 1 + i];
     }
 
     for (i = 0, j = 0, k = l; k <= r; k++){
         if((i < left_length) &&
-        (j >= right_length) || left[i] <= right[j])
+        (j >= right_length) || temp_left[i] <= temp_right[j])
         {
-            arr[k] = left[i];
+            arr[k] = temp_left[i];
             i++;
         } else {
-            arr[k] = right[j];
+            arr[k] = temp_right[j];
             j++;
         }
     }
 
-    free(left);
-    free(right);
 }
 
 int main(){
